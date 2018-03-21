4c5b2f27cd3edca1f325f2ea22e099a887fac8d1
Julien Chiquet <julien.chiquet@gmail.com>
2018-03-21 11:56:12 +0100

    figures PR

--- R/motivations.R
+++ R/motivations.R
@@ -58,22 +58,47 @@ for (n0 in c(p/2, p, 2*p)) {
   out.all <- rbind(out.all, out)
 }
 
+library(plyr)
 library(dplyr)
+library(tidyr)
+
 
 dplot.all <- out.all %>% 
   mutate(rmse = sqrt(error.mse), 
           precision = ifelse(tp == 0, 0, tp /(tp + fp)),
           recall = ifelse(tp == 0, 0, tp /(tp + fn))) %>% 
   mutate(f_measure = ifelse ( (precision == 0 | recall == 0), 0,  2 * (precision * recall) / (precision + recall))) %>% 
-  rename(hamming = error.sup, timings = times, accuracy = prec) %>% 
+  dplyr::rename(hamming = error.sup, timings = times, accuracy = prec) %>% 
   select(-error.cla, -error.mse, -steps, -package) %>% 
   filter(method != "glmnet high")
 
-dplot <- dplot.all %>% select("lambda", "method", "np.ratio", "precision", "recall", "rmse") %>% gather(key = "measure", value = "value", "rmse", "precision", "recall")
+dplot_precision <- dplot.all %>% select("lambda", "method", "np.ratio", "precision") %>% gather(key = "measure", value = "value", "precision")
+dplot_recall    <- dplot.all %>% select("lambda", "method", "np.ratio", "recall"   ) %>% gather(key = "measure", value = "value", "recall")
+dplot_rmse      <- dplot.all %>% select("lambda", "method", "np.ratio", "rmse"     ) %>% gather(key = "measure", value = "value", "rmse")
 
-p <- ggplot(dplot, aes(x = lambda, y = value, colour = method, group = method, lty = method)) +
+p_precision <- ggplot(dplot_precision, aes(x = lambda, y = value, colour = method, group = method, lty = method)) +
   stat_summary(fun.data="mean_cl_normal", geom = "smooth", alpha = 0.1) +
-  coord_trans(x = "log10") + labs(x="", y="") +
-  facet_grid(measure ~ np.ratio, scales = "free") + theme(legend.position="none") + theme_minimal()
+  coord_trans(x = "log10") + labs(x="", y="") + theme_minimal(base_size = 14) + theme(legend.position = "none", strip.text.x = element_blank()) + 
+  facet_grid(. ~ np.ratio) 
 
-ggsave(filename = "../figures/accuracy.pdf", width = 10, height = 7)
+p_recall <- ggplot(dplot_recall, aes(x = lambda, y = value, colour = method, group = method, lty = method)) +
+  stat_summary(fun.data="mean_cl_normal", geom = "smooth", alpha = 0.1) +
+  coord_trans(x = "log10") + labs(x="", y="") + theme_minimal(base_size = 14)  + theme(legend.position = c(.1,.1), strip.text.x = element_blank()) +
+  facet_grid(. ~ np.ratio) 
+
+p_rmse <- ggplot(dplot_rmse, aes(x = lambda, y = value, colour = method, group = method, lty = method)) +
+  stat_summary(fun.data="mean_cl_normal", geom = "smooth", alpha = 0.1) +
+  coord_trans(x = "log10") + labs(x="", y="") + theme_minimal(base_size = 14) + theme(legend.position = "none", strip.text.x = element_blank()) +
+  facet_grid(. ~ np.ratio) 
+
+res_PR <- ddply(dplot.all, .(lambda, method, np.ratio), summarise, precision = mean(precision), recall = mean(recall))
+p_PR <- ggplot(res_PR, aes(x=recall, y = precision, group=method, colour=method, lty=method)) + geom_line() + 
+  facet_grid(.~np.ratio) + theme_minimal(base_size = 14) + labs(x="", y="") + theme(legend.position = "none", strip.text.x = element_blank())
+
+
+  
+ggsave(plot = p_precision, filename = "../figures/precision.pdf", width = 10, height = 3)
+ggsave(plot = p_recall   , filename = "../figures/recall.pdf", width = 10, height = 3)
+ggsave(plot = p_rmse     , filename = "../figures/rmse.pdf", width = 10, height = 3)
+ggsave(plot = p_PR       , filename = "../figures/PR_curves.pdf", width = 10, height = 3)
+
